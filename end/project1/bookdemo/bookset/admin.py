5d3674a02d6bc8399b153a1faee8c76d2e02e515
cxw001 <973855683@qq.com>
2020-02-13 11:41:09 +0800

    自定义管理界面和关联注册

--- end/project1/bookdemo/bookset/admin.py
+++ end/project1/bookdemo/bookset/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
-
+from django.contrib.admin import ModelAdmin
 # Register your models here.
 
 from .models import Book, Hero
-admin.site.register(Book)
-admin.site.register(Hero)
+
+
+class HeroInline(admin.StackedInline):
+    model = Hero
+    # 关联个数
+    extra = 1
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "pub_date")
+    list_filter = ("title", "price")
+    search_fields = ("title", "price")
+    list_per_page = 1
+    inlines = [HeroInline]
+
+
+class HeroAdmin(admin.ModelAdmin):
+    list_display = ("name", "gender", "content", "book")
+    list_filter = ("name", "gender", "book")
+    search_fields = ("name", "gender")
+    list_per_page = 1
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Hero, HeroAdmin)

5384d95b753162b6851abe97496e8c6f18186854
cxw001 <973855683@qq.com>
2020-03-02 00:30:44 +0800

    基于函数的视图

--- end/demo3/drfend/shop/views.py
+++ end/demo3/drfend/shop/views.py
@@ -21,30 +21,50 @@ from .models import *
 from .serializers import *
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
-from rest_framework.request import Request
+from rest_framework import status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 # 通过api_view装饰器可以将基于函数的视图转换为APIVIEW基于类的视图
 @api_view(["GET","POST"])
 def categoryList(request):
-    print(request,type(request))
     if request.method == "GET":
         print("获取到GET请求参数",request.query_params)
-        return HttpResponse("获取列表成功")
+        queryset = Category.objects.all()
+        seria = CategorySerializer(instance=queryset,many=True)
+        return Response(seria.data,status=status.HTTP_200_OK)
     elif request.method == "POST":
         print("获取到POST请求参数",request.data)
-        return HttpResponse("创建成功")
+        # data为序列化对象 来源于请求中提取的数据
+        seria = CategorySerializer(data=request.data)
+        # 从请求中提取的数据序列化之前要进行检验
+        if seria.is_valid():
+            seria.save()
+            return Response(data=seria.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(data=seria.errors,status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET","PUT","PATCH","DELETE"])
 def categoryDetail(request,cid):
+    model = get_object_or_404(Category,pk=cid)
     if request.method == "GET":
         print("获取到GET请求参数", request.query_params)
-        return HttpResponse("获取单个成功")
+        seria = CategorySerializer(instance=model)
+        return Response(seria.data,status=status.HTTP_200_OK)
     elif request.method == "PUT" or request.method == "PATCH":
-        print("获取到PUT/PATCH请求参数",request.data)
-        return HttpResponse("修改成功")
+        # 更新就是从请求中提取参数 替换数据库中提取的数据
+        seria = CategorySerializer(instance=model,data=request.data)
+        # 验证是否合法
+        if seria.is_valid():
+            seria.save()
+            return Response(seria.data,status=status.HTTP_200_OK)
+        else:
+            return Response(seria.errors,status=status.HTTP_400_BAD_REQUEST)
     elif request.method == "DELETE":
-        return HttpResponse("删除成功")
+        model.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
     else:
         return HttpResponse("当前路由不允许"+request.method+"操作")
 
